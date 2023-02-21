742d0407db6a998820a6b9f4ae95669ac27c0942
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-21 21:40:50 +0100

    Add defaults

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./style.scss";
 
 const promptStr = ">> ";
+let fileHandle;
 
 const input = document.getElementById('input') as HTMLTextAreaElement;
 const output = document.getElementById('output') as HTMLDivElement;
@@ -18,6 +19,8 @@ input.addEventListener('keydown', (e) => {
 
         if (command === 'clear') {
             output.innerHTML = '';
+        } else if (command === '') {
+            output.innerHTML += "---<br />";
         } else {
             const result = processCommand(command);
             output.innerHTML += `${promptStr}${command}<br />${result.replace("\n", "<br />")}<br />`;
@@ -36,6 +39,13 @@ const processCommand = (command: string): string => {
             Type \`help name\` to find out more about the function \`name\`.`;
         case "ls":
             return "/";
+//        case "load":
+//            fileHandle = window.showOpenFilePicker().then(handle => {
+//                console.log(handle);
+//                console.log(handle[0].getFile());
+//            });
+//            console.log(fileHandle);
+//            return "/";
         default:
             return "Command does not exist.";
     }
