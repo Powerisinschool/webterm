f2f3101083e5e23324c2db9fc21c861da7065e87
Tolulope Olagunju <65245607+Powerisinschool@users.noreply.github.com>
2023-04-12 21:24:44 +0100

    update mail link in contact

--- src/commands/contact.ts
+++ src/commands/contact.ts
@@ -1,5 +1,5 @@
 export default function ContactFn(): string {
-    return `You can get in touch with me via email at toolulopeolagunju@gmail.com or connect with me on LinkedIn at <a href="https://www.linkedin.com/in/tolulope-olagunju-11b8361a9/">https://www.linkedin.com/in/tolulope-olagunju-11b8361a9/</a>.
+    return `You can get in touch with me via email at <a href="mailto:toolulopeolagunju@gmail.com">toolulopeolagunju@gmail.com</a> or connect with me on LinkedIn at <a href="https://www.linkedin.com/in/tolulope-olagunju-11b8361a9/">https://www.linkedin.com/in/tolulope-olagunju-11b8361a9/</a>.
 
 I'm always happy to chat about software engineering, web development, or any other related topics, so don't hesitate to reach out if you have any questions or want to discuss a project.
 
